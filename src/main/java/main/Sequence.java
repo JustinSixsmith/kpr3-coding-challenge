baecce0673dea733d0e9fb22a3f17b3c8b09aa69
Justin Sixsmith <justinsixsmith@gmail.com>
2022-06-22 14:05:48 -0500

    building out phrase creation

--- src/main/java/main/Sequence.java
+++ src/main/java/main/Sequence.java
@@ -1,7 +1,32 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Sequence {
+    public static void main(String[] args) {
+
+//        System.out.println(createPhrases(args[0]));
+        System.out.println(createPhrases("It's a trap!"));
 
 
+    }
+
+
+
+    private static ArrayList<String> createPhrases(String text) {
+        // Split text into individual words, separated by space
+        String[] words = text.split(" ");
+        // Initialize phrase for each three word sequence
+        StringBuilder phrase = new StringBuilder();
+        // Initialize list for three word phrases
+        ArrayList<String> phrases = new ArrayList<>();
+        // Create three word phrases from words, in sequence
+        for (int i = 0; i == words.length - 1; i++) {
+            phrase.append(words[i]);
+        }
+        phrases.add(phrase.toString());
+        return phrases;
+    }
 
 }
--- src/test/java/main/SequenceTest.java
+++ src/test/java/main/SequenceTest.java
@@ -1,7 +1,16 @@
 package main;
 
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
 public class SequenceTest {
+
+//    @Test
+//    public void sayHello() {
+//        assertEquals("Hello, Tom", sayHello("Tom"));
+//    }
+
+
 }
