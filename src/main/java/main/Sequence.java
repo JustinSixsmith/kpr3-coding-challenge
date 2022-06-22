f4bba38d3b3ee5cbf48f3ca1e7893d157e4f605d
Justin Sixsmith <justinsixsmith@gmail.com>
2022-06-22 17:04:49 -0500

    refactoring phrase creator

--- src/main/java/main/Sequence.java
+++ src/main/java/main/Sequence.java
@@ -7,26 +7,23 @@ public class Sequence {
     public static void main(String[] args) {
 
 //        System.out.println(createPhrases(args[0]));
-        System.out.println(createPhrases("It's a trap!"));
+        createPhrases("It's a trap that I cannot!");
+
 
 
     }
 
 
 
-    private static ArrayList<String> createPhrases(String text) {
+    private static void createPhrases(String text) {
         // Split text into individual words, separated by space
         String[] words = text.split(" ");
-        // Initialize phrase for each three word sequence
+
         StringBuilder phrase = new StringBuilder();
-        // Initialize list for three word phrases
-        ArrayList<String> phrases = new ArrayList<>();
-        // Create three word phrases from words, in sequence
-        for (int i = 0; i == words.length - 1; i++) {
-            phrase.append(words[i]);
+        for (int i = 0; i < words.length - 3; i++) {
+            phrase.append(words[i] + " ");
         }
-        phrases.add(phrase.toString());
-        return phrases;
+        System.out.println(phrase);
     }
 
 }
