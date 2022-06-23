715cbbb86ce890a5378415ce0063947a8a9bc93c
Justin Sixsmith <justinsixsmith@gmail.com>
2022-06-23 13:51:03 -0500

    removes spaces, punctuation, and changes to lower case

--- src/main/java/main/Sequence.java
+++ src/main/java/main/Sequence.java
@@ -1,35 +1,52 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 public class Sequence {
     public static void main(String[] args) {
 
-//        System.out.println(createPhrases(args[0]));
-        System.out.println(createPhraseArray("It's a trap that I cannot avoid!"));
+        System.out.println(createPhraseArray("It's a trap, that I cannot avoid! It's a trap,"));
 
     }
 
 
     public static ArrayList<String> createPhraseArray(String text) {
-
-        // Split text into individual words, separated by space
+        // Split text into individual words
         String[] words = text.split(" ");
 
+        // Initialize array of three sequential word phrases
         ArrayList<String> phrases = new ArrayList<>();
+
+        // Iterate over words array
         for (int i = 0; i < words.length - 2; i++) {
-            phrases.add(words[i] + " " + words[i + 1] + " " + words[i + 2]);
+
+            // Trim spaces around each word, make lower case, and remove punctuation
+            String trimWord1 = words[i].toLowerCase().trim().replaceAll("[^a-z ]", "");
+            String trimWord2 = words[i + 1].toLowerCase().trim().replaceAll("[^a-z ]", "");
+            String trimWord3 = words[i + 2].toLowerCase().trim().replaceAll("[^a-z ]", "");
+
+            // Create new three words strings
+            String phrase = trimWord1 + " " + trimWord2 + " " + trimWord3;
+
+            // Add to the array
+            phrases.add(phrase);
         }
 
+        // Create new array just for duplicate strings
+        Set<String> duplicates = new HashSet<>();
+
         for (String phrase : phrases) {
-            if (phrase.length() == 11) {
+            if (!duplicates.add(phrase)) {
                 System.out.println("ONE");
             }
         }
 
         return phrases;
     }
+
 
 
 
