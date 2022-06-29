910b624dfcb6ff949d7b77b0c9d8818ca5e65b63
Justin Sixsmith <justinsixsmith@gmail.com>
2022-06-29 11:58:55 -0500

    adds tests for ignore case sesnitivity and removing punctuation

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -7,13 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
     public static final ArrayList<String> TEST_ARRAY = new ArrayList<>(Arrays.asList("this is a", "is a test"));
+
     @Test
     void main() {
     }
 
     @Test
-    void createPhraseArray() {
-        String testString = "This is a test.";
+    void makeLowerCaseTest() {
+        String testString = "THIS IS A TEST";
+        assertEquals(TEST_ARRAY, Solution.createPhraseArray(testString));
+    }
+
+    @Test
+    void removePunctuationTest() {
+        String testString = "Th!is. is&* a Test#@";
+        assertEquals(TEST_ARRAY, Solution.createPhraseArray(testString));
+    }
+
+    @Test
+    void ignoreNextLineTest() {
+        String testString = "This is a\ntest.";
         assertEquals(TEST_ARRAY, Solution.createPhraseArray(testString));
     }
 
