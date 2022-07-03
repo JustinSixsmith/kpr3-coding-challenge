fbaf48acafc11e922d5f644ad526e5db47d9db2e
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-03 08:54:18 -0500

    adds text to readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # kpr3-coding-challenge
-# kpr3-coding-challenge
-# kpr3-coding-challenge
+
+## Find the 100 most common three word sequences in descending order.
+1. Open the command terminal and navigate to the root directory of the project (kp3-coding-challenge).
+2. From here, be sure to first compile the code with the command: `javac src/main/java/Solution.java`
+3. 
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -4,18 +4,19 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
-    public static final ArrayList<String> TEST_ARRAY = new ArrayList<>(Arrays.asList("this is a", "is a test"));
+    public static final LinkedList<String> TEST_ARRAY = new LinkedList<>(Arrays.asList("this is a", "is a test"));
     public static final String SIMPLE_TEXT_PATH = "src/main/resources/texts/simple.txt";
     public static final String TWO_WORD_PATH = "src/main/resources/texts/2-word-catcher.txt";
     public static final String[] SINGLE_TEST_ARGS = {SIMPLE_TEXT_PATH};
-    public static final String[] TEST_ARGS = {SIMPLE_TEXT_PATH, TWO_WORD_PATH};
+    public static final String[] MULTI_TEST_ARGS = {SIMPLE_TEXT_PATH, TWO_WORD_PATH};
     public static final List<File> SINGLE_TEST_FILE = new ArrayList<>();
-    public static final List<File> TEST_FILES = new ArrayList<>();
+    public static final List<File> MULTI_TEST_FILES = new ArrayList<>();
 
     @Before
     public void setUp() {
@@ -29,18 +30,51 @@ class SolutionTest {
 
     @Test
     void createAMultipleFilesListTest() {
-        TEST_FILES.add(new File(SIMPLE_TEXT_PATH));
-        TEST_FILES.add(new File(TWO_WORD_PATH));
-        assertEquals(TEST_FILES, Solution.addFilePaths(TEST_ARGS));
+        MULTI_TEST_FILES.add(new File(SIMPLE_TEXT_PATH));
+        MULTI_TEST_FILES.add(new File(TWO_WORD_PATH));
+        assertEquals(MULTI_TEST_FILES, Solution.addFilePaths(MULTI_TEST_ARGS));
     }
 
     @Test
-    void removeSingleQuotesTest() {
+    void asserThrowsFileNotFoundTest() {
+
+    }
+
+//    @Test
+//    void removePunctuationTest() {
+//        String sampleString = "T@^his is$^ a test!";
+//        String[] expected = {"this", "is", "a", "test"};
+//        assertEquals(expected, Solution.separateWords(sampleString));
+//    }
+//
+//    @Test
+//    void makeLowerCaseTest() {
+//        String sampleString = "THIS IS A TEST";
+//        String[] expected = {"this", "is", "a", "test"};
+//        assertEquals(expected, Solution.separateWords(sampleString));
+//    }
+
+    @Test
+    void removeBeginningQuoteTest() {
+        String expected = "can't";
+        assertEquals(expected, Solution.removeSingleQuotes("'can't"));
+    }
+
+    @Test
+    void removeEndingQuoteTest() {
+        String expected = "can't";
+        assertEquals(expected, Solution.removeSingleQuotes("can't'"));
+    }
+
+    @Test
+    void removeBothBeginningAndEndingQuotesTest() {
         String expected = "can't";
         assertEquals(expected, Solution.removeSingleQuotes("'can't'"));
     }
 
-    @Test
-    void countPhrases() {
-    }
+//    @Test
+//    void countPhrases() {
+//
+//        assertEquals(expected, Solution.countPhrases(TEST_ARRAY));
+//    }
 }
